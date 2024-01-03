b9844b2af0e59963776a04edd46881c40c363c0c
USER <USER@DESKTOP-TV8AAGO>
2024-01-03 22:57:52 +0600

    all added

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 class Bizcard extends StatelessWidget {
@@ -8,16 +10,67 @@ class Bizcard extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text("bizcard"),
+        backgroundColor: Colors.lightBlue,
+        centerTitle: true,
       ),
       body: Container(
         alignment: Alignment.center,
-        child: const Stack(
-          alignment: Alignment.center,
+        child:  Stack(
+          alignment: Alignment.topCenter,
           children: [
-            Text("hello"),
+           _getcard(),
+           _getavatar()
+
           ],
         ),
+       
       ),
     );
+  }
+  
+  Container _getcard() {
+    return  Container(
+      width: 350,
+      height: 200,
+      margin: EdgeInsets.all(50),
+      decoration:  BoxDecoration(color: Color.fromARGB(255, 41, 151, 14),
+      borderRadius: BorderRadius.circular(10)
+    ),
+child: Column(
+  mainAxisAlignment: MainAxisAlignment.center,
+   children: [
+    const Text("Mamun ur rashid",),
+    
+    
+    Text("mamun@gmail.com"),
+    Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(Icons.add_home_work),Text("my home work")
+      ],
+    )
+    
+  ],
+),
+
+
+    
+    );
+
+  }
+  
+  Container _getavatar() {
+return Container(
+  width: 100,
+  height: 100,
+  decoration: BoxDecoration(
+    color: Colors.amber,
+    borderRadius: BorderRadius.all(Radius.circular(50)),
+    border: Border.all(color: Colors.white30,width: 1.2,),
+    image: DecorationImage(image: NetworkImage("https://picsum.photos/300/300"),
+    fit:BoxFit.cover
+  ),
+)
+);
   }
 }
